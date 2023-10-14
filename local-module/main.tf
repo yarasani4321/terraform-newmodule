0cad06f4fac957d7ef4db7324e132548d4e0860d
root <root@ip-172-31-81-206.ec2.internal>
2023-10-14 16:06:34 +0000

    new

---
+++ local-module/main.tf
@@ -0,0 +1,31 @@
+resource aws_instance test {
+    ami=data.aws_ami.ami.id
+    instance_type=var.instance_type
+    vpc_security_group_ids=[var.security_group]
+    tags={
+      Name=var.name
+    }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = var.zone_id
+  name    = "${var.name}.yarasani.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.test.private_ip]
+}
+resource "null_resource" "ansible" {
+
+  depends_on = [
+    aws_route53_record.www
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /root/roboshop-ansible
+git pull
+sleep 30
+ansible-playbook -i ${var.name}.yarasani.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+EOF
+  }
+}
